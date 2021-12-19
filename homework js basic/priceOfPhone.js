556080f10a573a5149637180e1701a595d006e18
dNikolikj <96394836+dNikolikj@users.noreply.github.com>
2021-12-19 22:41:29 +0100

    Add files via upload

---
+++ homework js basic/priceOfPhone.js
@@ -0,0 +1,6 @@
+
+
+//homework
+let priceOfOnePhone = (119.95 * 5) / 100 + 119.95;
+let priceOfThirtyPhones = priceOfOnePhone * 30 
+console.log(`The price of 30 phones is ${priceOfThirtyPhones} dollars.`);
