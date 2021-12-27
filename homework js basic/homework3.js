7fb2ed7a9ccdc46b37de396b005fe824f9688e72
dNikolikj <96394836+dNikolikj@users.noreply.github.com>
2021-12-27 17:36:05 +0100

    Add files via upload

---
+++ homework js basic/homework3.js
@@ -0,0 +1,70 @@
+// exercise 1
+
+function myNumber(a, b) {
+    let sum = a * b
+    console.log(sum);
+}
+
+myNumber(6, 7);
+myNumber(9, 4);
+
+function myString() {
+   console.log('my string');
+}
+myString();
+
+function myBoolean (num1 , num2) {
+let action = num1 > num2 ;
+console.log(action);
+if(num1 > num2) {
+    print("true");
+}
+else {
+    print("false");
+}
+}
+myBoolean(5, 9);
+
+undefined i object ne mi se jasni!
+//  function undefined
+// function myObject
+// function unDefined
+
+
+
+
+
+// exercise #2
+
+// function ageConverter(type, number) {
+     
+//  if(type === "Dog") {
+//      return number * 7;
+//  }
+//  else if(type === "Human") {
+//      return number / 7;
+//  }
+//  else {
+//  return "invalid input"
+// }
+// }
+
+// let userTypeInput = prompt("Enter Dog if you want to check how old is your dog, vice versa enter Human to see how old are you compare to your dog");
+// let userNumberinput = parseInt(prompt("Enter a certain age number"));
+// alert(ageConverter(userTypeInput, userNumberinput));
+ 
+exercise 3 ne mi e jasna!
+// function yourAtmAccount( number1 = 1000, number2) {
+//     let result = number1 - number2; 
+//     console.log( result, "you have left dollars on your account");
+//     if(result <= 1000) {
+//     print(result);
+// }
+// else {
+// return "invalid input"
+// }
+// let numberInput = parseInt(prompt("Enter number:"));
+// alert(yourAtmAccount(numberInput));
+// yourAtmAccount= (number1, 500);
+
+// }
